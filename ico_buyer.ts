8d8151531a4410b3b7daa3aa34111ef80c49b7ff
Morgan <c5ipo7i@yahoo.com>
2021-03-16 14:03:04 -0700

    feat: changed filetype to ts. added maximum profit and maximum gas price willing to pay

---
+++ ico_buyer.ts
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const { utils } = require("web3");
+const { API } = require("./trading_class");
+const cron = require("node-cron");
+const Tokens = require("./tokens.js");
+const { Token, Pair } = require("@uniswap/sdk");
+var BN = utils.BN;
+const CHAINID = 4;
+const CHAIN_DICT = {
+  1: "MAINNET",
+  4: "RINKEBY",
+};
+const api = new API(
+  process.env.DEFAULT_ACCOUNT,
+  process.env.PRIVATE_KEY,
+  CHAINID
+);
+const tokenName = "DAI";
+const inputAmount = 1;
+const gasUsed = 125000;
+console.log(inputAmount);
+
+const buybotbuy = cron.schedule("* * * * * *", async () => {
+  const balance = await api.balance();
+  const maxProfit = balance * 2;
+  const liquidityBool = await api.checkLiquidity(tokenName);
+  if (liquidityBool) {
+    const highestPending = await api.getHighestPrice();
+    const memPendGasHigh = utils.fromWei(highestPending.gasPrice, "gwei");
+    const maxGasPrice = utils.toBN(maxProfit) / (utils.toBN(gasUsed) * 1000);
+    console.log("highestPending.gasPrice", highestPending.gasPrice);
+    console.log("Gas high in gwei", memPendGasHigh);
+    console.log("Maximum gas price willing to pay", maxGasPrice, maxGasPrice);
+    const predETHTxFee = memPendGasHigh * gasUsed; // Use to determine whether or not tx is worth it.
+    const gasPrice = utils.fromWei(highestPending.gasPrice, "gwei");
+    console.log(predETHTxFee);
+    let res = api.swapFromEth(inputToken, inputAmount, gasPrice);
+    if (typeof res !== "error") {
+      stopbuybot();
+    }
+  }
+});
+
+function stopbuybot() {
+  // buybotbuy.
+}
+
+const testTrade = async (token) => {};
