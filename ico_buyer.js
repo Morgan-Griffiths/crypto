9d30793a2c96a68f64f6b991bb01bc1863d8d8de
Alex Lewis <alexandroslewis@gmail.com>
2021-03-16 13:19:13 -0700

    added buyer added seer added opening logic for sell and cancel

--- ico_buyer.js
+++ ico_buyer.js
@@ -1,31 +1,54 @@
-require('dotenv').config()
+require("dotenv").config();
 const { utils } = require("web3");
-const { API } = require('./trading_class');
-const cron = require('node-cron');
-const api = new API(process.env.DEFAULT_ACCOUNT, process.env.PRIVATE_KEY, 1);
-const inputToken = process.argv[process.argv.length-1];
-const inputAmount = process.argv[process.argv.length-1];
+const { API } = require("./trading_class");
+const cron = require("node-cron");
+const api = new API(process.env.DEFAULT_ADDRESS, process.env.PRIVATE_KEY, 1);
+const inputToken = process.argv[process.argv.length - 2];
+const inputAmount = process.argv[process.argv.length - 1];
 const gasUsed = 125000;
 
-const buybotbuy = cron.schedule('* * * * * *', async() => {
-    const liquidityBool = await api.checkLiquidity(inputToken);
-    if (liquidityBool) {
-        const highestPending = await api.getHighestPrice()
-        const memPendGasHigh = utils.fromWei(highestPending.gasPrice, 'ether');
-        const predETHTxFee = memPendGasHigh * gasUsed; // Use to determine whether or not tx is worth it.
-        const gasPrice = utils.fromWei(highestPending.gasPrice, 'gwei');
-        console.log(predETHTxFee)
-        // let res = api.swapFromEth(inputToken, inputAmount, gasPrice);
-        // if(typeof res !== 'error') {
-        //     stopbuybot();
-        // }
-    }
+const checkbotcheck = cron.schedule("* * * * * *", async () => {
+  const liquidityBool = await api.checkLiquidity(inputToken);
+  if (liquidityBool) {
+    buybotbuy();
+  }
 });
 
-function stopbuybot() {
-    // buybotbuy.
+async function buybotbuy() {
+  try {
+    checkbotcheck.destroy();
+    const highestPending = await api.getHighestPrice();
+    const memPendGasHigh = utils.fromWei(highestPending.gasPrice, "ether");
+    const predETHTxFee = memPendGasHigh * gasUsed; // Use to determine whether or not tx is worth it.
+    const gasPrice = utils.fromWei(highestPending.gasPrice, "gwei");
+    await api.swapFromEth(inputToken, inputAmount, gasPrice);
+    const myCurrTxHash = await api.getMyTxHash();
+    if (typeof myCurrTxHash === "error") {
+        throw new Error(myCurrTxHash.message);
+    }
+    let timer = 0;
+    const seebotsee = cron.schedule("* * * * * *", async () => {
+      timer++;
+      const receipt = api.getMyReceipt(myCurrTxHash);
+      if (timer >= 15 && !receipt) {
+          cancelTx(seebotsee);
+      }
+      if (timer < 15 && receipt.status) {
+          sellbotsell(seebotsee);
+      }
+    });
+  } catch (e) {
+      console.log(e.message);
+      seebotsee.destroy();
+  }
 }
 
-const testTrade = async(token) => {
-
+function sellbotsell(cron) {
+    cron.destroy();
 }
+
+function cancelTx(cron) {
+    cron.destroy();
+}
+
+const testTrade = async (token) => {};
